fcc6126a94071a72b281de1be7eababb07d52fc5
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2026-08-18 17:33:19 -0600

    fix: configura tags en español

---
+++ lang/vendor/filament-pwa/es/messages.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'sections' => [
+        'general' => 'Configuración general',
+        'style' => 'Apariencia',
+        'icons' => 'Iconos',
+        'splash' => 'Pantallas de inicio',
+        'shortcuts' => 'Accesos directos',
+    ],
+
+    'settings' => [
+        'title' => 'Aplicación web progresiva',
+        'description' => 'Configura la aplicación web progresiva',
+    ],
+
+    'form' => [
+        'pwa_app_name' => 'Nombre de la aplicación',
+        'pwa_short_name' => 'Nombre corto',
+        'pwa_start_url' => 'URL de inicio',
+
+        'pwa_background_color' => 'Color de fondo',
+        'pwa_theme_color' => 'Color del tema',
+        'pwa_status_bar' => 'Color de la barra de estado',
+        'pwa_display' => 'Modo de visualización',
+        'pwa_orientation' => 'Orientación',
+
+        'pwa_icons_72x72' => 'Icono de 72 × 72',
+        'pwa_icons_96x96' => 'Icono de 96 × 96',
+        'pwa_icons_128x128' => 'Icono de 128 × 128',
+        'pwa_icons_144x144' => 'Icono de 144 × 144',
+        'pwa_icons_152x152' => 'Icono de 152 × 152',
+        'pwa_icons_192x192' => 'Icono de 192 × 192',
+        'pwa_icons_384x384' => 'Icono de 384 × 384',
+        'pwa_icons_512x512' => 'Icono de 512 × 512',
+
+        'pwa_splash_640x1136' => 'Pantalla de inicio de 640 × 1136',
+        'pwa_splash_750x1334' => 'Pantalla de inicio de 750 × 1334',
+        'pwa_splash_828x1792' => 'Pantalla de inicio de 828 × 1792',
+        'pwa_splash_1125x2436' => 'Pantalla de inicio de 1125 × 2436',
+        'pwa_splash_1242x2208' => 'Pantalla de inicio de 1242 × 2208',
+        'pwa_splash_1242x2688' => 'Pantalla de inicio de 1242 × 2688',
+        'pwa_splash_1536x2048' => 'Pantalla de inicio de 1536 × 2048',
+        'pwa_splash_1668x2224' => 'Pantalla de inicio de 1668 × 2224',
+        'pwa_splash_1668x2388' => 'Pantalla de inicio de 1668 × 2388',
+        'pwa_splash_2048x2732' => 'Pantalla de inicio de 2048 × 2732',
+
+        'pwa_shortcuts' => 'Accesos directos',
+        'pwa_shortcuts_name' => 'Nombre',
+        'pwa_shortcuts_description' => 'Descripción',
+        'pwa_shortcuts_url' => 'URL',
+        'pwa_shortcuts_icon' => 'Icono',
+    ],
+];
